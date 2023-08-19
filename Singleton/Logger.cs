aab60b8212e98ef70adc8ae5f9278f57f7b5362d
ar.hafizi <ar.hafizi@OWJ-12>
2023-08-19 16:06:06 +0330

    Singleton Implementation

---
+++ Singleton/Logger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton;
+
+//thread Safe version
+public class TSLogger {
+    private static readonly Lazy<TSLogger> _lazyLogger = new(() => new TSLogger());
+
+    private TSLogger() {
+    }
+
+    public static TSLogger? Instance {
+        get {
+            return _lazyLogger.Value;
+        }
+    }
+    //singleton operation
+    public void Log(string message) {
+        Console.WriteLine($"Loging --- {message}");
+    }
+}
+//
+public class Logger {
+    private static Logger? _instance { get; set; }
+
+    private Logger() {
+    }
+
+    public static Logger? Instance {
+        get {
+            if (_instance == null) {
+                _instance = new();
+                return _instance;
+            }
+            return _instance;
+        }
+    }
+    //singleton operation
+    public void Log(string message) {
+        Console.WriteLine($"Loging --- {message}");
+    }
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Singleton;
+using System;
+
+Console.Title = "Singleton Pattern";
+
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if(instance1 == instance2 && instance2 == Logger.Instance) {
+    Console.WriteLine("Instances are the same");
+}
+
+instance1.Log($"message from {nameof(instance1)}");
+instance2.Log($"message from {nameof(instance2)}");
+
+Logger.Instance.Log($"message from ::: {nameof(Logger.Instance)}");
