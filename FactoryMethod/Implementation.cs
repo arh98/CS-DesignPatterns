9f8150b76e7daaa2e02bc88c722007dd6debe909
ar.hafizi <ar.hafizi@OWJ-12>
2023-08-20 16:51:49 +0330

    Factory method Implementation

---
+++ FactoryMethod/Implementation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod;
+
+//abs factory method
+public abstract class DiscountService {
+    public abstract int DiscountPercentage { get; }
+    public override string ToString() => GetType().Name;
+}
+// Concrete factory method 1
+public class CountryDiscountService : DiscountService {
+    private readonly string _countryId;
+    public CountryDiscountService(string countryId) {
+        _countryId = countryId;
+    }
+    public override int DiscountPercentage {
+        get {
+            return _countryId switch {
+                "IR" => 30,
+                _ => 10
+            };
+        }
+    }
+}
+//Concrete factory method 2
+public class CodeDiscountService : DiscountService {
+    private readonly Guid _code;
+
+    public CodeDiscountService(Guid code) {
+        _code = code;
+    }
+    public override int DiscountPercentage {
+        //...
+        get => 15;
+    }
+}
+
+//abs creator
+public abstract class DiscountFactory {
+    public abstract DiscountService CreateDiscountService();
+}
+
+//concrete creator 1 
+public class CountryDiscountFactory : DiscountFactory {
+    private readonly string _countryId;
+    public CountryDiscountFactory(string countryId) {
+        _countryId = countryId;
+    }
+    public override DiscountService CreateDiscountService() {
+        return new CountryDiscountService(_countryId);
+    }
+}
+// concrete creator 2
+public class CodeDiscountFactory : DiscountFactory {
+    private readonly Guid _code;
+    public CodeDiscountFactory(Guid code) {
+        _code = code;
+    }
+    public override DiscountService CreateDiscountService() {
+        return new CodeDiscountService(_code);
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,11 @@
+﻿using FactoryMethod;
+
+var factories = new List<DiscountFactory> {
+new CodeDiscountFactory(Guid.NewGuid()),
+new CountryDiscountFactory("IR")
+};
+
+foreach (var factory in factories) {
+    var discountService = factory.CreateDiscountService();
+    Console.WriteLine($"Persentage  : {discountService.DiscountPercentage}\nComming from : {discountService}");
+}
