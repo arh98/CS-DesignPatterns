41c03b389e8ca7a993fbab0cb9e39750edc28492
ahmadrz <a.hafizi9@outlook.com>
2023-09-02 15:39:30 +0330

    protoType implementation

---
+++ Prototype/Implamentation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype;
+
+//ProtoType : 
+public abstract class Person {
+    public abstract string Name { get; set; }
+    public abstract Person Clone();
+}
+
+//concrete-ProtoType 1
+public class Manager : Person {
+    public override string Name { get; set; }
+    public Manager(string name) {
+        Name = name;
+    }
+    public override Person Clone() {
+        //create a shallow copy of existing obj
+        return (Person)MemberwiseClone();
+    }
+}
+
+//concrete-ProtoType 2
+public class Employee : Person {
+    public override string Name { get; set; }
+    public Manager Manager { get; set; }
+    public Employee(string name, Manager manager) {
+        Name = name;
+        Manager = manager;
+    }
+    public override Person Clone() {
+        return (Person)MemberwiseClone();
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,12 @@
+﻿using Prototype;
+
+var manager = new Manager("cindy");
+var managerClone = (Manager)manager.Clone();
+Console.WriteLine($"Manager was cloned : {managerClone.Name}");
+
+var employee = new Employee("alex" , managerClone);
+var employeeClone = (Employee)employee.Clone();
+Console.WriteLine($"employee was cloned : {employeeClone.Name} -- manager : {employeeClone.Manager.Name}");
+
+managerClone.Name = "jack";
+Console.WriteLine($"employee was cloned : {employeeClone.Name} -- manager : {employeeClone.Manager.Name}");
