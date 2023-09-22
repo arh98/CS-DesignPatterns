820ad31e05d67f1df9fb3dd4074f9f0ec10fdcdf
ahmadrz <a.hafizi9@outlook.com>
2023-09-22 16:03:50 +0330

    Composite Implementation

---
+++ Composite/Composite.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite;
+
+//component
+public abstract class FileSystem {
+    public string Name { get; set; }
+    public abstract long GetSize();
+    public FileSystem(string name) {
+        Name = name;
+    }
+}
+
+//leaf
+public class File : FileSystem {
+    private long _size;
+
+    public File(string name, long size) : base(name) {
+        _size = size;
+    }
+
+    public override long GetSize() {
+        return _size;
+    }
+}
+
+//composite
+public class Directory : FileSystem {
+    private long _size;
+    private List<FileSystem> _items;
+    public Directory(string name, int v) : base(name) {
+        _items = new List<FileSystem>();
+    }
+
+    public override long GetSize() {
+        var treeSize = _size;
+        foreach (FileSystem fs in _items) {
+            treeSize += fs.GetSize();
+        }
+        return treeSize;
+    }
+    public void Add(FileSystem fs) {
+        _items.Add(fs);
+    }
+    public void Remove(FileSystem fs) {
+        _items.Remove(fs);
+    }
+
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Title = "Composite-Pattern";
+
+var root = new Composite.Directory("root", 0);
+var topLevelFile = new Composite.File("tfile.txt", 40);
+var topLevelDir1 = new Composite.Directory("topLevelDirectory1", 4);
+var topLevelDir2 = new Composite.Directory("topLevelDirectory2", 4);
+
+root.Add(topLevelFile);
+root.Add(topLevelDir1);
+root.Add(topLevelDir2);
+
+var subLevelFile1 = new Composite.File("sublevel1.txt", 200);
+var subLevelFile2 = new Composite.File("sublevel2.txt", 150);
+
+topLevelDir2.Add(subLevelFile1);
+topLevelDir2.Add(subLevelFile2);
+
+Console.WriteLine($"Size of topLevelDirectory1: {topLevelDir1.GetSize()}");
+Console.WriteLine($"Size of topLevelDirectory2: {topLevelDir2.GetSize()}");
+Console.WriteLine($"Size of root: {root.GetSize()}");
+
+Console.ReadKey();
